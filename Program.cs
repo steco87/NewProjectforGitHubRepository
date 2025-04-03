9dee7a47452fbc7f33d5b96ab448ce710c0a8161
steco87 <steco_87@yahoo.com>
2025-04-03 11:48:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace New_Project_for_Git_Hub_Repository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
