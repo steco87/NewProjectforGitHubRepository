826d53f9d41aac521fa751bfe849cc94d65fd40d
steco87 <steco_87@yahoo.com>
2025-04-03 11:53:36 +0300

    First commit for wWriteline message updates

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, THis is a new project for git hub repository, branches, commit!");
         }
     }
 }
